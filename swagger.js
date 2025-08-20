16773352c1299bd2cdaa564a2c5f224e6ac8fd26
JuanCamiloFernandezSalinas <juan.fernandez@smartdevproject.com>
2025-08-20 07:50:13 -0500

    dEV

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       { url: 'http://localhost:3001', description: 'Local' },
-      { url: 'https://backend-lottoapp-ijsj.onrender.com/', description: 'Producción' },
+      { url: 'https://backend-lottoapp-ijsj.onrender.com', description: 'Producción' },
 
     ],
   },
